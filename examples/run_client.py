f7d28143101f8f727546c63d3108beaf01781052
Jacky Liang <jackyliang@cmu.edu>
2021-09-16 13:28:43 -0400

    minor revisions

--- examples/run_client.py
+++ examples/run_client.py
@@ -1,11 +1,12 @@
 '''This script is a non-runnable example - you need to provide your own model class and wandb info
 '''
-
+import logging
 from nn_service import NNClient
 
 
 if __name__ == '__main__':
-    client = NNClient('127.0.0.1', '5555', '/tmp/pyarrow')
+    logging.getLogger().setLevel(logging.INFO)
+    client = NNClient('127.0.0.1', '5555', '/tmp/plasma')
     client.register(model_cls_name, cache_dir, run_path, checkpoint, gpu)
 
     y = client.query(model_cls_name, {'x': x})['y']
--- examples/run_server.py
+++ examples/run_server.py
@@ -1,9 +1,10 @@
 '''This script is a non-runnable example - you need to provide your own model class and wandb info
 '''
-
+import logging
 from nn_service import NNServer
 
 
 if __name__ == '__main__':
-    server = NNServer('127.0.0.1', '5555', '/tmp/pyarrow', {model_cls.__name__: model_cls})
+    logging.getLogger().setLevel(logging.INFO)
+    server = NNServer('127.0.0.1', '5555', '/tmp/plasma', {model_cls.__name__: model_cls})
     server.serve()
